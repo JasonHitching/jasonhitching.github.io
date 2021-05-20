01cf2f7097f9a6ddfb5aac7e9e1731b7c97b1faa
Jason Hitching <jasonhitching1994@gmail.com>
2021-05-20 16:25:37 +0100

    font adjustments

--- src/views/BlogHome.vue
+++ src/views/BlogHome.vue
@@ -3,16 +3,18 @@
     <div v-for="(post, index) in posts" :key="post.slug + '_' + index">
       <b-row>
         <b-col class="mx-auto col-lg-8 col-md-10">
-          <b-card class="mb-5 border-0">
+          <b-card class="border-0">
             <router-link :to="'/blog/' + post.slug">
-              <b-card-title class="mt-3">{{ post.title }}</b-card-title>
-              <b-card-text>
+              <b-card-title class="h2 text-dark mt-4 font-weight-bold">{{
+                post.title
+              }}</b-card-title>
+              <b-card-text class="text-monospace text-secondary">
                 {{ post.summary }}
               </b-card-text>
-              <small class="text-muted">{{ post.published }}</small>
+              <small class="font-italic text-muted">{{ post.published }}</small>
             </router-link>
           </b-card>
-          <hr class="blog-hr" />
+          <hr class="mx-auto blog-hr" />
         </b-col>
       </b-row>
     </div>
@@ -53,5 +55,10 @@ hr.blog-hr {
   height: 0;
   border-top: 1px solid rgba(0, 0, 0, 0.1);
   border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+}
+
+.card-body {
+  padding: 0;
+  margin-bottom: 24px;
 }
 </style>
