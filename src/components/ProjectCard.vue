e07f70f381911e766b31050acae8777dec840072
Jason Hitching <jasonhitching1994@gmail.com>
2021-12-06 19:28:52 +0000

    VueBootstrap card swapped to a Bootstrap card template
    - Swapped due to restrictions surrounding the VueBootstrap card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,24 +1,27 @@
 <template>
-  <b-card
-    title="Card Title"
-    img-src="https://picsum.photos/600/300/?image=25"
-    img-alt="Image"
-    img-top
-    tag="article"
-    style="max-width: 20rem; background-color: #232734"
-    class="mb-2 text-white"
-  >
-    <b-card-text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </b-card-text>
-
-    <b-button href="#" class="bg-success">Go somewhere</b-button>
-  </b-card>
+  <div class="card text-white" style="width: 20rem; background-color: #232734">
+    <img
+      src="https://picsum.photos/600/300/?image=25"
+      class="card-img-top"
+      alt="#"
+    />
+    <div class="card-body">
+      <h5 class="card-title">My project</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+      <span class="badge rounded-pill bg-white text-success">Java</span>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+.proj-card {
+  border-radius: 100px;
+}
+</style>
