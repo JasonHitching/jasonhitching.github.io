0f2f8d76a031af94fc1c7ca520ab6868ad6dbc95
Jason Hitching <jasonhitching1994@gmail.com>
2021-12-06 15:46:38 +0000

    created project section vue component
    - Project section will display card views of personal projects on the home screen

---
+++ src/components/ProjectSection.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-row>
+      <b-col class="d-flex justify-content-center"><project-card /> </b-col>
+      <b-col class="d-flex justify-content-center"><project-card /> </b-col>
+      <b-col class="d-flex justify-content-center"><project-card /> </b-col
+    ></b-row>
+  </div>
+</template>
+
+<script>
+import ProjectCard from "../components/ProjectCard.vue";
+export default {
+  name: "ProjectSection",
+  components: {
+    ProjectCard
+  }
+};
+</script>
+
+<style></style>
