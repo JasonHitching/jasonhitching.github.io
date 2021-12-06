19e12b969848dd36d95fa1dabf9b2f9204850197
Jason Hitching <jasonhitching1994@gmail.com>
2021-12-06 15:48:11 +0000

    created project card vue component
    - Project card component will be rendered to display seperate projects
    - Made up of a bootstrap card

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem; background-color: #232734"
+    class="mb-2 text-white"
+  >
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </b-card-text>
+
+    <b-button href="#" class="bg-success">Go somewhere</b-button>
+  </b-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
