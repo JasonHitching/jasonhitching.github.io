c140875b69840638b24cc61cdb9b188aea88da4b
Jason Hitching <jasonhitching1994@gmail.com>
2021-12-20 13:58:05 +0000

    fixed: end of input error

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/jasonhitching.github.io/'
-    : '/'
-
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/jasonhitching.github.io/" : "/"
+};
