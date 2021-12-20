747e6f193dd1fc8d115fe0be69c5a175eb94ab9e
Jason Hitching <jasonhitching1994@gmail.com>
2021-12-20 13:47:00 +0000

    modified: ProjectSection save location, created ContactSection template

---
+++ src/views/ProjectSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <b-row class="d-flex justify-content-center space-between pt-4 pb-3">
+      <b-col lg="3"
+        ><project-card
+          cardTitle="My Portfolio"
+          cardText="Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Consectetur adipiscing elit."
+          :techUsed="['Vue.js', 'Bootstrap']"
+        />
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import ProjectCard from "../components/ProjectCard.vue";
+export default {
+  name: "ProjectSection",
+  components: {
+    ProjectCard
+  }
+};
+</script>
+
+<style>
+.space-between {
+  gap: 80px;
+}
+</style>
